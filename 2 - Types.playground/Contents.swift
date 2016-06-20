bdfcade3ec86dd4daae6cfe47c13d19635ff9f2e
Slimikus <slimikus@gmail.com>
2016-06-20 14:32:23 +0300

    Создание файлов 2 урока

---
+++ 2 - Types.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// переменные
+var str = "Hello, playground"
+str = "New value"
+
+// константы
+let pi = 3.14159262
+
+
+// Типы данных
+
+var integer: Int = 1
+var integer2: Int = 2
+
+integer + integer2
+
+var f: Float = 1.23
+var d: Double = 1.23
+
+let c = f/(Float(d))
+c*2
+
+var myString: String = "My String"
+var my2String = "my 2 String"
+myString + my2String
+
+
