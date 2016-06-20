937006c4e2bbde692ea6b8e3d9e2bc45e83f452c
Slimikus <slimikus@gmail.com>
2016-06-20 16:07:32 +0300

    длбавлены Множества и Массивы

--- 2 - Types.playground/Contents.swift
+++ 2 - Types.playground/Contents.swift
@@ -25,6 +25,35 @@ c*2
 
 var myString: String = "My String"
 var my2String = "my 2 String"
-myString + my2String
+myString + " - " + my2String
+myString.stringByReplacingOccurrencesOfString("y", withString: "u")
+
+
+var b: Bool = true
+b = false
+
+var b2: Bool = 1==1
+
+let data = NSDate()
+
+
+// Множества
+
+var set: Set = ["Стол","Стул","Диван"]
+set.insert("Лампа")
+set.insert(str)
+var removeObj = set.remove("Стол")
+removeObj
+
+// Массивы
+
+var array = [1,2,3,4,5,"123", NSData()]
+var array2: Array<String> = ["1","2","3"]
+
+var arayOBJC = NSArray(objects: 1,2,3,4)
+
+var arayOBJC_Mutable = NSMutableArray(objects: 1,2,3,4)
+arayOBJC_Mutable
+
 
 
