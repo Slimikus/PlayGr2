61cd6d172bfc359dda7c6cc49d75727849ce98d6
Slimikus <slimikus@gmail.com>
2016-06-29 15:05:43 +0300

    Опциональные типы

---
+++ 3 - Опциональные типы.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+var option: String? = "My string"
+var s = "My super string"
+
+//option = nil
+
+if option != nil {
+    print(option!)
+} else {
+    print("option is nil")
+}
+
+if let o = option {
+    print(o)
+} else {
+    print("option is nil")
+}
+
+var i: Int!
+i = nil
+i = 123
+
+var ii = i
+
+
