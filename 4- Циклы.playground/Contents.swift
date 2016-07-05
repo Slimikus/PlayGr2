e0c73ac118b411e1c7f54c1cb8ce27c27045e5b5
Slimikus <slimikus@gmail.com>
2016-07-05 09:31:17 +0300

    операторы FOR

--- 4- Циклы.playground/Contents.swift
+++ 4- Циклы.playground/Contents.swift
@@ -1,12 +1,34 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+var str = "Hello, playground"
+
+// Цикл For
+
+for var i = 0; i <= 10; i+=1 {
+    print(i)
+}
+
+for i in 1...9 {
+    i
+}
+
+for s in str.characters {
+    var c = String(s)
+}
 
 
+var array = ["Anna", "Gulia", "Yulia", "Sveta"]
+for girl in array {
+    girl
+}
 
-
-
-
+var dict = ["Den": 20, "Maria": 18, "Petr": 32]
+var summaryAge = 0
+for (key, value) in dict {
+    summaryAge = summaryAge + value
+}
+summaryAge
 
 
 
