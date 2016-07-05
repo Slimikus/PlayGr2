af2d2941f56f98ab7a14866f102272e996700630
Slimikus <slimikus@gmail.com>
2016-07-05 09:41:29 +0300

    Оператор if

--- 5, 6 - Условные операторы.playground/Contents.swift
+++ 5, 6 - Условные операторы.playground/Contents.swift
@@ -4,4 +4,19 @@ import UIKit
 
 var str = "Hello, playground"
 
+if str == "test" {
+    str = "1"
+}  else if str == "Hello, playground" {
+    "Hello"
+} else {
+    str = "2"
+}
 
+
+var karmaPoints = 10
+var isArtionPositiv = false
+if isArtionPositiv {
+    karmaPoints += 2
+} else {
+    karmaPoints -= 1
+}
