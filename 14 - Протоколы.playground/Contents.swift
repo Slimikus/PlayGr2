bc3d86d9e260c018903625c2cb33e42794ca9472
Slimikus <slimikus@gmail.com>
2016-07-15 12:07:57 +0300

    Протоколы

--- 14 - Протоколы.playground/Contents.swift
+++ 14 - Протоколы.playground/Contents.swift
@@ -4,6 +4,37 @@ import UIKit
 
 var str = "Hello, playground"
 
+protocol GirlProtocol {
+    var name: String {set get}
+    var age: Int {set get}
+    
+    func sayHello(boyName: String) -> String
+    func sendSms(phoneNumber: String, text: String) -> Bool
+    
+    init(name: String, age: Int)
+}
+
+class Girl: GirlProtocol {
+    var name: String
+    var age: Int
+    
+    func sayHello(boyName: String) -> String {
+        return "Hi, \(boyName)"
+    }
+    func sendSms(phoneNumber: String, text: String) -> Bool {
+        //
+        return true
+    }
+    
+    required init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
+
+let girl = Girl(name: "Hanna", age: 18)
+girl.sayHello("Petya")
+
 
 
 
