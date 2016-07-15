bdc07d4017f00a7d1c8a1f75bdd4f3547856f7b8
Slimikus <slimikus@gmail.com>
2016-07-15 11:18:39 +0300

    11. Наследование классов

--- 10-13 - Классы.playground/Contents.swift
+++ 10-13 - Классы.playground/Contents.swift
@@ -6,11 +6,15 @@ var str = "Hello, playground"
 
 // Инициализация
 class Human {
-    var name: String
+    var name: String?
     var age: Int
     
     func sayHello(nameFriend: String) -> String {
         return "Hello, \(nameFriend)! It's your friend \(self.name)."
+    }
+    
+    init() {
+        age = 1
     }
     
     init(name: String, age: Int) {
@@ -28,7 +32,32 @@ h1.age
 h1.name
 h1.sayHello("Dima")
 
+// Наследование
 
+class Girl: Human {
+    var hairColor: UIColor
+    
+    override func sayHello(nameFriend: String) -> String {
+        if let name = name {
+            return "Hi, \(nameFriend). My name is \(name)" }
+        return "Hi, \(nameFriend)."
+    }
+    
+    override init() {
+        hairColor = UIColor.whiteColor()
+        super.init(name: "Anna", age: 18)
+    }
+    init(hc: UIColor){
+        hairColor = hc
+        super.init(name: "Anna", age: 18)
+    }
+    
+    
+}
+
+var girl1 = Girl(hc: UIColor.darkTextColor())
+girl1.name = nil
+girl1.sayHello("Petya")
 
 
 
