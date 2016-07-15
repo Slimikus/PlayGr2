5cefba88842d3884f90cdec0db767778587a082b
Slimikus <slimikus@gmail.com>
2016-07-15 10:52:03 +0300

    10. Инициализация классов

--- 10-13 - Классы.playground/Contents.swift
+++ 10-13 - Классы.playground/Contents.swift
@@ -3,3 +3,39 @@
 import UIKit
 
 var str = "Hello, playground"
+
+// Инициализация
+class Human {
+    var name: String
+    var age: Int
+    
+    func sayHello(nameFriend: String) -> String {
+        return "Hello, \(nameFriend)! It's your friend \(self.name)."
+    }
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
+
+var h1 = Human(name: "Andrey", age: 33)
+var h2 = h1
+
+h2.name = "Oleg"
+
+h1.age
+h1.name
+h1.sayHello("Dima")
+
+
+
+
+
+
+
+
+
+
+
+
