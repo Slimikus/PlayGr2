5a78e295116b60e6ab723c0b8de0d0842468b35f
Slimikus <slimikus@gmail.com>
2016-07-15 10:23:05 +0300

    рассмотрение темы "Структуры"

--- 9 - Структуры.playground/Contents.swift
+++ 9 - Структуры.playground/Contents.swift
@@ -3,3 +3,38 @@
 import UIKit
 
 var str = "Hello, playground"
+
+struct Point {
+    var x: Int
+    var y: Int
+    
+    func printPoint() -> String {
+        return "x = \(x); y = \(y)"
+    }
+    
+    mutating func resetpoint() {
+        x = 0
+        y = 0
+    }
+    
+    init() {
+        x = 0
+        y = 0
+    }
+    
+    init(x: Int, y: Int) {
+        self.x = x
+        self.y = y
+    }
+}
+
+var p = Point(x: 10, y: 10)
+
+p.x
+p.y
+p.resetpoint()
+p.printPoint()
+
+
+
+
